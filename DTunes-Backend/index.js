54659441e91dce1f484c83769be99946c6dfa8c0
Nikhil <nikhilsaini746@gmail.com>
2024-07-01 20:36:59 +0530

    initial code

---
+++ DTunes-Backend/index.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const JwtStrategy = require('passport-jwt').Strategy,
+    ExtractJwt = require('passport-jwt').ExtractJwt;
+
+const passport = require('passport');
+const User = require('./models/User');
+
+require('dotenv').config();
+const app = express();
+const port = 3000;
+
+const password = process.env.MONGO_PASSWORD;
+// const URI = "mongodb+srv://nikhilsaini735510:"+password+"@cluster0.kigtde0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const URI = `mongodb+srv://admin:${password}@cluster0.kigtde0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+mongoose.connect(URI,{
+    useNewUrlParser : true,
+    useUnifiedTopology : true,
+}).then(()=>{
+    console.log("connected to Mongo");
+}).catch(()=>{
+    console.log("not connected to mongo");
+});
+
+
+
+// authentication using passport will be removed and auth using your own manual way will be there just using passport for now only later will remove
+
+let opts = {}
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = 'anyRandomSecretKeyUseEnvVar';
+passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
+    User.findOne({id: jwt_payload.sub}, function(err, user) {
+        if (err) {
+            return done(err, false);
+        }
+        if (user) {
+            return done(null, user);
+        } else {
+            return done(null, false);
+            // or you could create a new account
+        }
+    });
+}));
+
+
+
+app.get("/",(req,res)=>{
+    res.send("Hello World");
+})
+
+app.listen(port,()=>{
+    console.log("App is running on port    " +port);
+})
---
+++ DTunes-Backend/models/PlayList.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const PlayList = new mongoose.Schema({
+    name : {
+        type : String,
+        required : true
+    },
+    thumbnail : {
+        type : String,
+        required : true
+    },
+    owner : {
+        type : mongoose.Types.ObjectId,
+        ref : "User"
+    },
+    songs :[
+        {
+        type : mongoose.Types.ObjectId,
+        ref : "Song"
+        }
+        ],
+    collaborators : [
+        {
+            type : mongoose.Types.ObjectId,
+            ref : "User"
+        }
+    ]
+
+});
+
+const PlayListModel = mongoose.model("PlayList",PlayList);
+
+module.exports = PlayListModel;
